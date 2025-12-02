03e9703dab7b4725de1519f85ccc823ee1cc7dee
Darrell Pelan <dpelan@woh.rr.com>
2025-12-01 22:54:05 -0500

    .net wierdness

--- Test/obj/Debug/net8.0/Test.AssemblyInfo.cs
+++ Test/obj/Debug/net8.0/Test.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd1d9c9516291b40edce6924568417248e22e73f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+905b62e3906fe0ef90b1ce0dd523dcd3baed348b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
